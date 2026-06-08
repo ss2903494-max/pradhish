439d0b4d00cb64fa9894ace29601288a664d5db5
pradhish <ss2903494@gmail.com>
2026-06-08 15:03:42 +0530

    first commit

--- src/day3.java
+++ src/day3.java
@@ -1,13 +1,18 @@
 void main(){
     int[] arr = {87,32,44,89,32};
-    System.out.println(arr[4]);
-    for(int i=0;i<=4;i++) {
-        System.out.println(arr[i]);
-        int val = arr[i];
-        if (val >= 40) {
-            System.out.println("Pass");
-        } else {
-            System.out.println("Fail");
+    System.out.println("Pass mark: ");
+    for (int i=0;i<=4;i++){
+        int val=arr[i];
+        if(val > 40){
+            System.out.println(val+" ");
+
+        }
+    }
+    System.out.println("\nFail mark: ");
+    for (int i=0;i<=4;i++){
+        int val=arr[i];
+        if(val < 40){
+            System.out.println(val+" ");
         }
     }
 }
